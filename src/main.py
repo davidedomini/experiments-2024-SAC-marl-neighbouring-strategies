80b3fe42d3086c9d85e616bc8eca375505ed19ef
Davide Domini <davide.domini@unibo.it>
2024-10-01 18:02:28 +0200

    add import of all methods in main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+from training.nn_averaging import DTDE_nn_averaging
+from training.nn_consensus import DTDE_nn_consensus
+from training.nn_weigh_averaging import DTDE_nn_weigh_averaging
+from training.experience_sharing import DTDE_experience_sharing
 from training.DTDE import DTDE
 from pathlib import Path
 import time
@@ -7,10 +11,14 @@ if __name__ == "__main__":
 
     results_dir = 'data/'
     Path(results_dir).mkdir(parents=True, exist_ok=True)
-    for seed in range(10):
+    for seed in range(1, 10):
         print(f"Start of training with seed {seed}")
         start = time.time()
-        DTDE(seed)
+        # DTDE(seed)
+        # DTDE_nn_averaging(seed)
+        DTDE_nn_consensus(seed)
+        DTDE_nn_weigh_averaging(seed)
+        DTDE_experience_sharing(seed)
         end = time.time()
         print(f"End of training with seed {seed}")
         print(f"Took {end - start} seconds")
