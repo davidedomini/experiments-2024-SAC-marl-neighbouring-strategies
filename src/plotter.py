dc0753877a6f335f0fee73a83b566afe2c270083
Davide Domini <davide.domini@unibo.it>
2024-10-03 12:22:58 +0200

    add plotting of comparison for episode len in eval

--- src/plotter.py
+++ src/plotter.py
@@ -2,57 +2,112 @@ import os
 import glob
 from pathlib import Path
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+from utils.metrics import load_json_as_dict
 
 def read_csv_files(directory, experiment):
-
     csv_files = glob.glob(f"{directory}*{experiment}*.csv")
     data_frames = []
-
     for file in csv_files:
         df = pd.read_csv(file)
         data_frames.append(df)
-    
     return data_frames
 
 
 def compute_mean_variance(data_frames, column):
     all_data = pd.concat([df[column] for df in data_frames], axis=1)
-
     mean_values = all_data.mean(axis=1)
     variance_values = all_data.var(axis=1)
-    
     return mean_values, variance_values
 
 
-def plot(data, metric):
-    plt.figure(figsize=(12, 8))
+def beautify_metric(metric):
+    if metric == 'episode_reward_mean':
+        return 'Mean Episode Reward'
+    elif metric == 'episode_len_mean':
+        return 'Mean Episode Length'
+    else:
+        return 'Unknown metric'
 
-    #colors_v = sns.color_palette("colorblind", 10) 
+
+def beautify_label(label):
+    if label == 'CTDE':
+        return 'CTDE' 
+    elif label == 'DTDE':
+        return 'DTDE'
+    elif label == 'NN-averaging':
+        return 'NN Averaging'
+    elif label == 'NN-consensus':
+        return 'NN Consensus'
+    elif label == 'experience-sharing':
+        return 'Experience Sharing'
+    else:
+        return 'Unknown label'
+
+
+def plot(data, metric):
+    plt.figure(figsize=(10, 6))
     viridis = plt.colormaps['viridis']
     indexes = np.linspace(0.1, 0.9, len(data))
-
     for i, (mean, variance, exp) in enumerate(data):
-        plt.plot(mean, label=exp, color=viridis(indexes[i]))
+        plt.plot(mean, label=beautify_label(exp), color=viridis(indexes[i]))
         upper_bound = mean + np.sqrt(variance)
         lower_bound = mean - np.sqrt(variance)
         plt.fill_between(mean.index, lower_bound, upper_bound, color=viridis(indexes[i]), alpha=0.2)
-
     plt.xlabel('Episode time')
-    plt.ylabel(metric)
-    plt.title(metric)
+    m = beautify_metric(metric)
+    plt.ylabel(m)
+    # plt.title(m)
     plt.legend()
     plt.savefig(f"charts/{metric}.pdf")
+    plt.close()
 
 
+def plot_mean_time_comparison_methods():
+    
+    for n_agents in [4, 8, 16, 32]:
+        # plt.figure(figsize=(40, 30))
+        data_ctde = load_json_as_dict(f"data/eval/collect_the_items-algo=DQN&method=CTDE_v3_2908--agents={n_agents}&items=30&spawn_area=500")
+        data_nn_averaging = load_json_as_dict(f"data/eval/collect_the_items-algo=DQN&method=DTDE[02]_v2_2908--agents={n_agents}&items=30&spawn_area=500")
+        data_nn_consensus = load_json_as_dict(f"data/eval/collect_the_items-algo=DQN&method=DTDE[03]_v2_2908--agents={n_agents}&items=30&spawn_area=500")
+        data_experience_sharing = load_json_as_dict(f"data/eval/collect_the_items-algo=DQN&method=DTDE[05]_v2_2908--agents={n_agents}&items=30&spawn_area=500")
+        data_dtde = load_json_as_dict(f"data/eval/collect_the_items-algo=DQN&method=DTDE[01]_v0--agents={n_agents}&items=30&spawn_area=500")
+
+        data = [(data_ctde, 'CTDE'), (data_dtde, 'DTDE'), (data_nn_averaging, 'NN Averaging'), (data_nn_consensus, 'NN Consensus'), (data_experience_sharing, 'Experience Sharing')]
+
+        fig, ax = plt.subplots()
+        viridis = plt.colormaps['viridis']
+        indexes = np.linspace(0.1, 0.9, len(data))
+
+        for i, (data, experiment) in enumerate(data):
+            ax.boxplot(data,
+            positions=[i],
+            notch=True,
+            label=experiment,
+            widths=0.5,
+            patch_artist=True, boxprops={"facecolor": viridis(indexes[i])},)
+        plt.legend(loc='upper right')
+        plt.ylabel('Mean Episode Length')
+        plt.title(f'{n_agents} Agents')
+        plt.tight_layout()
+        plt.savefig(f"charts/mean-time-comparison-{n_agents}-agents.pdf")
+        plt.close()
+
+
+def plot_mean_time_comparison_n_agents():
+    ...
+
+matplotlib.rcParams.update({'axes.titlesize': 20})
+matplotlib.rcParams.update({'axes.labelsize': 18})
+matplotlib.rcParams.update({'xtick.labelsize': 15})
+matplotlib.rcParams.update({'ytick.labelsize': 15})
 
 charts_dir = 'charts/'
 Path(charts_dir).mkdir(parents=True, exist_ok=True)
 
 experiments = ['CTDE', 'DTDE', 'NN-averaging', 'NN-consensus', 'experience-sharing']
-
 data_reward = []
 data_ep_len = []
 
@@ -65,3 +120,5 @@ for experiment in experiments:
 
 plot(data_reward, 'episode_reward_mean')
 plot(data_ep_len, 'episode_len_mean')
+plot_mean_time_comparison_methods()
+plot_mean_time_comparison_n_agents()
